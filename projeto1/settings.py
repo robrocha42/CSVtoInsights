a61e18e2dae88757e37a8d60cf8fe522d58073df
Robson Rocha <134662030+robrocha42@users.noreply.github.com>
2023-11-26 16:11:37 -0300

    Update settings.py
    Removido ALLOWED_HOSTS da AWS

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = parser["BASE"]["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["13.58.131.198", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
